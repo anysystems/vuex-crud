5d0f791e13e837a608e3f99e39b2ef0875e4a179
David Montalvillo <dmontalvillo@anysystems.es>
2020-09-02 14:59:25 +0200

    refactor: Apply prettier(extension) formatting

--- src/index.js
+++ src/index.js
@@ -42,101 +42,101 @@ import createState from './vuex-crud/createState';
  * @return {Object} A Vuex module.
  */
 const createCrud = ({
-  idAttribute = 'id',
-  resource,
-  urlRoot,
-  customUrlFn = null,
-  state = {},
-  actions = {},
-  mutations = {},
-  getters = {},
-  client = defaultClient,
-  onFetchListStart = () => {},
-  onFetchListSuccess = () => {},
-  onFetchListError = () => {},
-  onFetchSingleStart = () => {},
-  onFetchSingleSuccess = () => {},
-  onFetchSingleError = () => {},
-  onCreateStart = () => {},
-  onCreateSuccess = () => {},
-  onCreateError = () => {},
-  onUpdateStart = () => {},
-  onUpdateSuccess = () => {},
-  onUpdateError = () => {},
-  onReplaceStart = () => {},
-  onReplaceSuccess = () => {},
-  onReplaceError = () => {},
-  onDestroyStart = () => {},
-  onDestroySuccess = () => {},
-  onDestroyError = () => {},
-  only = ['FETCH_LIST', 'FETCH_SINGLE', 'CREATE', 'UPDATE', 'REPLACE', 'DESTROY'],
-  parseList = res => res,
-  parseSingle = res => res,
-  parseError = res => res
+    idAttribute = 'id',
+    resource,
+    urlRoot,
+    customUrlFn = null,
+    state = {},
+    actions = {},
+    mutations = {},
+    getters = {},
+    client = defaultClient,
+    onFetchListStart = () => {},
+    onFetchListSuccess = () => {},
+    onFetchListError = () => {},
+    onFetchSingleStart = () => {},
+    onFetchSingleSuccess = () => {},
+    onFetchSingleError = () => {},
+    onCreateStart = () => {},
+    onCreateSuccess = () => {},
+    onCreateError = () => {},
+    onUpdateStart = () => {},
+    onUpdateSuccess = () => {},
+    onUpdateError = () => {},
+    onReplaceStart = () => {},
+    onReplaceSuccess = () => {},
+    onReplaceError = () => {},
+    onDestroyStart = () => {},
+    onDestroySuccess = () => {},
+    onDestroyError = () => {},
+    only = ['FETCH_LIST', 'FETCH_SINGLE', 'CREATE', 'UPDATE', 'REPLACE', 'DESTROY'],
+    parseList = (res) => res,
+    parseSingle = (res) => res,
+    parseError = (res) => res,
 } = {}) => {
-  if (!resource) {
-    throw new Error('Resource name must be specified');
-  }
+    if (!resource) {
+        throw new Error('Resource name must be specified');
+    }
 
-  let rootUrl;
+    let rootUrl;
 
-  /**
-   * Create root url for API requests. By default it is: /api/<resource>.
-   * Use custom url getter if given.
-   */
-  if (typeof customUrlFn === 'function') {
-    rootUrl = customUrlFn;
-  } else if (typeof urlRoot === 'string') {
-    rootUrl = ((url) => {
-      const lastCharacter = url.substr(-1);
+    /**
+     * Create root url for API requests. By default it is: /api/<resource>.
+     * Use custom url getter if given.
+     */
+    if (typeof customUrlFn === 'function') {
+        rootUrl = customUrlFn;
+    } else if (typeof urlRoot === 'string') {
+        rootUrl = ((url) => {
+            const lastCharacter = url.substr(-1);
 
-      return lastCharacter === '/' ? url.slice(0, -1) : url;
-    })(urlRoot);
-  } else {
-    rootUrl = `/api/${resource}`;
-  }
+            return lastCharacter === '/' ? url.slice(0, -1) : url;
+        })(urlRoot);
+    } else {
+        rootUrl = `/api/${resource}`;
+    }
 
-  return {
-    namespaced: true,
+    return {
+        namespaced: true,
 
-    state: createState({ state, only }),
+        state: createState({ state, only }),
 
-    actions: createActions({
-      actions,
-      rootUrl,
-      only,
-      client,
-      parseList,
-      parseSingle,
-      parseError
-    }),
+        actions: createActions({
+            actions,
+            rootUrl,
+            only,
+            client,
+            parseList,
+            parseSingle,
+            parseError,
+        }),
 
-    mutations: createMutations({
-      mutations,
-      idAttribute,
-      only,
-      onFetchListStart,
-      onFetchListSuccess,
-      onFetchListError,
-      onFetchSingleStart,
-      onFetchSingleSuccess,
-      onFetchSingleError,
-      onCreateStart,
-      onCreateSuccess,
-      onCreateError,
-      onUpdateStart,
-      onUpdateSuccess,
-      onUpdateError,
-      onReplaceStart,
-      onReplaceSuccess,
-      onReplaceError,
-      onDestroyStart,
-      onDestroySuccess,
-      onDestroyError
-    }),
+        mutations: createMutations({
+            mutations,
+            idAttribute,
+            only,
+            onFetchListStart,
+            onFetchListSuccess,
+            onFetchListError,
+            onFetchSingleStart,
+            onFetchSingleSuccess,
+            onFetchSingleError,
+            onCreateStart,
+            onCreateSuccess,
+            onCreateError,
+            onUpdateStart,
+            onUpdateSuccess,
+            onUpdateError,
+            onReplaceStart,
+            onReplaceSuccess,
+            onReplaceError,
+            onDestroyStart,
+            onDestroySuccess,
+            onDestroyError,
+        }),
 
-    getters: createGetters({ getters })
-  };
+        getters: createGetters({ getters }),
+    };
 };
 
 export default createCrud;
--- src/vuex-crud/createActions.js
+++ src/vuex-crud/createActions.js
@@ -1,262 +1,242 @@
-const createActions = ({
-  actions,
-  rootUrl,
-  client,
-  only,
-  parseList,
-  parseSingle,
-  parseError
-}) => {
-  const [
-    FETCH_LIST,
-    FETCH_SINGLE,
-    CREATE,
-    UPDATE,
-    REPLACE,
-    DESTROY
-  ] = ['FETCH_LIST', 'FETCH_SINGLE', 'CREATE', 'UPDATE', 'REPLACE', 'DESTROY'];
-  const crudActions = {};
-  const isUsingCustomUrlGetter = typeof rootUrl === 'function';
+const createActions = ({ actions, rootUrl, client, only, parseList, parseSingle, parseError }) => {
+    const [FETCH_LIST, FETCH_SINGLE, CREATE, UPDATE, REPLACE, DESTROY] = [
+        'FETCH_LIST',
+        'FETCH_SINGLE',
+        'CREATE',
+        'UPDATE',
+        'REPLACE',
+        'DESTROY',
+    ];
+    const crudActions = {};
+    const isUsingCustomUrlGetter = typeof rootUrl === 'function';
 
-  const urlGetter = ({
-    customUrl,
-    customUrlFnArgs,
-    id,
-    type
-  }) => {
-    if (typeof customUrl === 'string') {
-      return customUrl;
-    } else if (isUsingCustomUrlGetter) {
-      const argsArray = Array.isArray(customUrlFnArgs) ? customUrlFnArgs : [customUrlFnArgs];
-      const args = [id || null, type || null].concat(argsArray);
-      return rootUrl(...args);
+    const urlGetter = ({ customUrl, customUrlFnArgs, id, type }) => {
+        if (typeof customUrl === 'string') {
+            return customUrl;
+        } else if (isUsingCustomUrlGetter) {
+            const argsArray = Array.isArray(customUrlFnArgs) ? customUrlFnArgs : [customUrlFnArgs];
+            const args = [id || null, type || null].concat(argsArray);
+            return rootUrl(...args);
+        }
+
+        return id ? `${rootUrl}/${id}` : rootUrl;
+    };
+
+    if (only.includes(FETCH_LIST)) {
+        Object.assign(crudActions, {
+            /**
+             * GET /api/<resourceName>
+             *
+             * Fetch list of resources.
+             */
+            fetchList({ commit }, { config, customUrl, customUrlFnArgs = [] } = {}) {
+                commit('fetchListStart');
+
+                return client
+                    .get(urlGetter({ customUrl, customUrlFnArgs, type: FETCH_LIST }), config)
+                    .then((res) => {
+                        const parsedResponse = parseList(res);
+
+                        commit('fetchListSuccess', parsedResponse);
+
+                        return parsedResponse;
+                    })
+                    .catch((err) => {
+                        const parsedError = parseError(err);
+
+                        commit('fetchListError', parsedError);
+
+                        return Promise.reject(parsedError);
+                    });
+            },
+        });
     }
 
-    return id ? `${rootUrl}/${id}` : rootUrl;
-  };
+    if (only.includes(FETCH_SINGLE)) {
+        Object.assign(crudActions, {
+            /**
+             * GET /api/<resourceName>/:id
+             *
+             * Fetch single resource.
+             */
+            fetchSingle({ commit }, { id, config, customUrl, customUrlFnArgs = [] } = {}) {
+                commit('fetchSingleStart');
 
-  if (only.includes(FETCH_LIST)) {
-    Object.assign(crudActions, {
-      /**
-       * GET /api/<resourceName>
-       *
-       * Fetch list of resources.
-       */
-      fetchList({ commit }, { config, customUrl, customUrlFnArgs = [] } = {}) {
-        commit('fetchListStart');
+                return client
+                    .get(
+                        urlGetter({
+                            customUrl,
+                            customUrlFnArgs,
+                            type: FETCH_SINGLE,
+                            id,
+                        }),
+                        config
+                    )
+                    .then((res) => {
+                        const parsedResponse = parseSingle(res);
 
-        return client.get(urlGetter({ customUrl, customUrlFnArgs, type: FETCH_LIST }), config)
-          .then((res) => {
-            const parsedResponse = parseList(res);
+                        commit('fetchSingleSuccess', parsedResponse);
 
-            commit('fetchListSuccess', parsedResponse);
+                        return res;
+                    })
+                    .catch((err) => {
+                        const parsedError = parseError(err);
 
-            return parsedResponse;
-          })
-          .catch((err) => {
-            const parsedError = parseError(err);
+                        commit('fetchSingleError', parsedError);
 
-            commit('fetchListError', parsedError);
+                        return Promise.reject(parsedError);
+                    });
+            },
+        });
+    }
 
-            return Promise.reject(parsedError);
-          });
-      }
-    });
-  }
+    if (only.includes(CREATE)) {
+        Object.assign(crudActions, {
+            /**
+             * POST /api/<resourceName>
+             *
+             * Create a new reource.
+             */
+            create({ commit }, { data, config, customUrl, customUrlFnArgs = [] } = {}) {
+                commit('createStart');
 
-  if (only.includes(FETCH_SINGLE)) {
-    Object.assign(crudActions, {
-      /**
-       * GET /api/<resourceName>/:id
-       *
-       * Fetch single resource.
-       */
-      fetchSingle({ commit }, {
-        id,
-        config,
-        customUrl,
-        customUrlFnArgs = []
-      } = {}) {
-        commit('fetchSingleStart');
+                return client
+                    .post(urlGetter({ customUrl, customUrlFnArgs, type: CREATE }), data, config)
+                    .then((res) => {
+                        const parsedResponse = parseSingle(res);
 
-        return client.get(urlGetter({
-          customUrl,
-          customUrlFnArgs,
-          type: FETCH_SINGLE,
-          id
-        }), config)
-          .then((res) => {
-            const parsedResponse = parseSingle(res);
+                        commit('createSuccess', parsedResponse);
 
-            commit('fetchSingleSuccess', parsedResponse);
+                        return parsedResponse;
+                    })
+                    .catch((err) => {
+                        const parsedError = parseError(err);
 
-            return res;
-          })
-          .catch((err) => {
-            const parsedError = parseError(err);
+                        commit('createError', parsedError);
 
-            commit('fetchSingleError', parsedError);
+                        return Promise.reject(parsedError);
+                    });
+            },
+        });
+    }
 
-            return Promise.reject(parsedError);
-          });
-      }
-    });
-  }
+    if (only.includes(UPDATE)) {
+        Object.assign(crudActions, {
+            /**
+             * PATCH /api/<resouceName>/:id
+             *
+             * Update a single resource.
+             */
+            update({ commit }, { id, data, config, customUrl, customUrlFnArgs = [] } = {}) {
+                commit('updateStart');
 
-  if (only.includes(CREATE)) {
-    Object.assign(crudActions, {
-      /**
-       * POST /api/<resourceName>
-       *
-       * Create a new reource.
-       */
-      create({ commit }, {
-        data,
-        config,
-        customUrl,
-        customUrlFnArgs = []
-      } = {}) {
-        commit('createStart');
+                return client
+                    .patch(
+                        urlGetter({
+                            customUrl,
+                            customUrlFnArgs,
+                            type: UPDATE,
+                            id,
+                        }),
+                        data,
+                        config
+                    )
+                    .then((res) => {
+                        const parsedResponse = parseSingle(res);
 
-        return client.post(urlGetter({ customUrl, customUrlFnArgs, type: CREATE }), data, config)
-          .then((res) => {
-            const parsedResponse = parseSingle(res);
+                        commit('updateSuccess', parsedResponse);
 
-            commit('createSuccess', parsedResponse);
+                        return parsedResponse;
+                    })
+                    .catch((err) => {
+                        const parsedError = parseError(err);
 
-            return parsedResponse;
-          })
-          .catch((err) => {
-            const parsedError = parseError(err);
+                        commit('updateError', parsedError);
 
-            commit('createError', parsedError);
+                        return Promise.reject(parsedError);
+                    });
+            },
+        });
+    }
 
-            return Promise.reject(parsedError);
-          });
-      }
-    });
-  }
+    if (only.includes(REPLACE)) {
+        Object.assign(crudActions, {
+            /**
+             * PUT /api/<resouceName>/:id
+             *
+             * Update a single resource.
+             */
+            replace({ commit }, { id, data, config, customUrl, customUrlFnArgs = [] } = {}) {
+                commit('replaceStart');
 
-  if (only.includes(UPDATE)) {
-    Object.assign(crudActions, {
-      /**
-       * PATCH /api/<resouceName>/:id
-       *
-       * Update a single resource.
-       */
-      update({ commit }, {
-        id,
-        data,
-        config,
-        customUrl,
-        customUrlFnArgs = []
-      } = {}) {
-        commit('updateStart');
+                return client
+                    .put(
+                        urlGetter({
+                            customUrl,
+                            customUrlFnArgs,
+                            type: REPLACE,
+                            id,
+                        }),
+                        data,
+                        config
+                    )
+                    .then((res) => {
+                        const parsedResponse = parseSingle(res);
 
-        return client.patch(urlGetter({
-          customUrl,
-          customUrlFnArgs,
-          type: UPDATE,
-          id
-        }), data, config)
-          .then((res) => {
-            const parsedResponse = parseSingle(res);
+                        commit('replaceSuccess', parsedResponse);
 
-            commit('updateSuccess', parsedResponse);
+                        return parsedResponse;
+                    })
+                    .catch((err) => {
+                        const parsedError = parseError(err);
 
-            return parsedResponse;
-          })
-          .catch((err) => {
-            const parsedError = parseError(err);
+                        commit('replaceError', parsedError);
 
-            commit('updateError', parsedError);
+                        return Promise.reject(parsedError);
+                    });
+            },
+        });
+    }
 
-            return Promise.reject(parsedError);
-          });
-      }
-    });
-  }
+    if (only.includes(DESTROY)) {
+        Object.assign(crudActions, {
+            /**
+             * DELETE /api/<resouceName>/:id
+             *
+             * Destroy a single resource.
+             */
+            destroy({ commit }, { id, config, customUrl, customUrlFnArgs = [] } = {}) {
+                commit('destroyStart');
 
-  if (only.includes(REPLACE)) {
-    Object.assign(crudActions, {
-      /**
-       * PUT /api/<resouceName>/:id
-       *
-       * Update a single resource.
-       */
-      replace({ commit }, {
-        id,
-        data,
-        config,
-        customUrl,
-        customUrlFnArgs = []
-      } = {}) {
-        commit('replaceStart');
+                return client
+                    .delete(
+                        urlGetter({
+                            customUrl,
+                            customUrlFnArgs,
+                            type: DESTROY,
+                            id,
+                        }),
+                        config
+                    )
+                    .then((res) => {
+                        const parsedResponse = parseSingle(res);
 
-        return client.put(urlGetter({
-          customUrl,
-          customUrlFnArgs,
-          type: REPLACE,
-          id
-        }), data, config)
-          .then((res) => {
-            const parsedResponse = parseSingle(res);
+                        commit('destroySuccess', id, parsedResponse);
 
-            commit('replaceSuccess', parsedResponse);
+                        return parsedResponse;
+                    })
+                    .catch((err) => {
+                        const parsedError = parseError(err);
 
-            return parsedResponse;
-          })
-          .catch((err) => {
-            const parsedError = parseError(err);
+                        commit('destroyError', parsedError);
 
-            commit('replaceError', parsedError);
+                        return Promise.reject(parsedError);
+                    });
+            },
+        });
+    }
 
-            return Promise.reject(parsedError);
-          });
-      }
-    });
-  }
-
-  if (only.includes(DESTROY)) {
-    Object.assign(crudActions, {
-      /**
-       * DELETE /api/<resouceName>/:id
-       *
-       * Destroy a single resource.
-       */
-      destroy({ commit }, {
-        id,
-        config,
-        customUrl,
-        customUrlFnArgs = []
-      } = {}) {
-        commit('destroyStart');
-
-        return client.delete(urlGetter({
-          customUrl,
-          customUrlFnArgs,
-          type: DESTROY,
-          id
-        }), config)
-          .then((res) => {
-            const parsedResponse = parseSingle(res);
-
-            commit('destroySuccess', id, parsedResponse);
-
-            return parsedResponse;
-          })
-          .catch((err) => {
-            const parsedError = parseError(err);
-
-            commit('destroyError', parsedError);
-
-            return Promise.reject(parsedError);
-          });
-      }
-    });
-  }
-
-  return Object.assign(crudActions, actions);
+    return Object.assign(crudActions, actions);
 };
 
 export default createActions;
